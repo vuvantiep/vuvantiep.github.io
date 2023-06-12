23c16ac4f4f6833525fc7acf06019d0c1aedd3b8
James Tran Van Quyen <quyen.tranvan.orc@ascentis.com.sg>
2023-06-12 15:19:09 +0700

    Build own NavPannel

---
+++ portfolio/SocialLinks.js
@@ -0,0 +1,27 @@
+function SolicalLinks() {
+  return (
+    <ul className="icons">
+      <li>
+        <a
+          href="https://www.linkedin.com/in/vu-tiep-b00525197/"
+          className="icon brands alt fa-linkedin"
+        >
+          <span className="label">LinkedIn</span>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://wa.me/qr/IY4YSZ45QFKKJ1"
+          className="icon brands alt fa-whatsapp"
+        >
+          <span className="label">whatsapp</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://t.me/tiepvv" className="icon brands alt fa-telegram">
+          <span className="label">Telegram</span>
+        </a>
+      </li>
+    </ul>
+  );
+}
